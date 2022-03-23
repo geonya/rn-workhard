986b291a5e814c9a6aacb70a86bb7d48b3621cc2
geonya <geony8410@gmail.com>
2022-03-23 13:01:29 +0900

    todo list challenge complete

--- App.js
+++ App.js
@@ -13,19 +13,40 @@ import {
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { theme } from "./color";
 import { Feather } from "@expo/vector-icons";
+import * as Progress from "react-native-progress";
 
-const STORAGE_KEY = "@toDos";
+const STORAGE_TODO_KEY = "@toDos";
+const STORAGE_TAB_KEY = "@tab";
 
 export default function App() {
 	const [working, setWorking] = useState(true);
 	const [text, setText] = useState("");
 	const [toDos, setToDos] = useState({});
-	const travle = () => setWorking(false);
-	const work = () => setWorking(true);
+	const [progress, setProgress] = useState();
+	const keyArray = Object.keys(toDos);
+	const travel = async () => {
+		setWorking(false);
+		await saveTab(false);
+	};
+	const work = async () => {
+		setWorking(true);
+		await saveTab(true);
+	};
 	const onChangeText = (payload) => setText(payload);
 	const saveToDos = async (toSave) => {
 		try {
-			await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(toSave));
+			await AsyncStorage.setItem(
+				STORAGE_TODO_KEY,
+				JSON.stringify(toSave)
+			);
+		} catch (e) {
+			console.log(e);
+			Alert.alert(e);
+		}
+	};
+	const saveTab = async (tab) => {
+		try {
+			await AsyncStorage.setItem(STORAGE_TAB_KEY, JSON.stringify(tab));
 		} catch (e) {
 			console.log(e);
 			Alert.alert(e);
@@ -33,25 +54,36 @@ export default function App() {
 	};
 	const loadToDos = async () => {
 		try {
-			const s = await AsyncStorage.getItem(STORAGE_KEY);
-			setToDos(JSON.parse(s));
+			const tab = await AsyncStorage.getItem(STORAGE_TODO_KEY);
+			setToDos(JSON.parse(tab));
 		} catch (e) {
 			console.log(e);
 			Alert.alert(e);
 		}
 	};
-	useEffect(() => {
-		loadToDos();
-	}, []);
+	const loadTab = async () => {
+		try {
+			const t = await AsyncStorage.getItem(STORAGE_TAB_KEY);
+			setWorking(JSON.parse(t));
+		} catch (e) {
+			console.log(e);
+			Alert.alert(e);
+		}
+	};
+
 	const addToDo = async () => {
 		if (text === "") {
 			return;
 		}
-		const newToDos = { ...toDos, [Date.now()]: { text, working } };
+		const newToDos = {
+			[Date.now()]: { text, working, done: false },
+			...toDos,
+		};
 		setToDos(newToDos);
 		await saveToDos(newToDos);
 		setText("");
 	};
+
 	const deleteToDo = async (id) => {
 		Alert.alert("Delete To Do?", "Are you sure?", [
 			{ text: "Cancel" },
@@ -67,30 +99,84 @@ export default function App() {
 			},
 		]);
 	};
+	const doneToDo = async (id) => {
+		const toDo = toDos[id];
+		const newToDo = toDo.done
+			? { ...toDo, done: false }
+			: { ...toDo, done: true };
+		const newToDos = { ...toDos, [id]: newToDo };
+		setToDos(newToDos);
+		await saveToDos(newToDos);
+	};
+	const changeToDo = async (id, text) => {
+		const toDo = toDos[id];
+		const newToDo = toDo.done ? { ...toDo, text } : { ...toDo, text };
+		const newToDos = { ...toDos, [id]: newToDo };
+		setToDos(newToDos);
+		await saveToDos(newToDos);
+	};
+	useEffect(() => {
+		loadToDos();
+		loadTab();
+	}, []);
+	useEffect(() => {
+		if (working) {
+			const totalToDos = keyArray.filter(
+				(key) => toDos[key].working
+			).length;
+			const totalDone = keyArray.filter(
+				(key) => toDos[key].working && toDos[key].done
+			).length;
+			setProgress(totalToDos === 0 ? 0 : totalDone / totalToDos);
+		} else {
+			const totalToDos = keyArray.filter(
+				(key) => !toDos[key].working
+			).length;
+			const totalDone = keyArray.filter(
+				(key) => !toDos[key].working && toDos[key].done
+			).length;
+			setProgress(totalToDos === 0 ? 0 : totalDone / totalToDos);
+		}
+	}, [working, toDos]);
 	return (
 		<View style={styles.container}>
 			<StatusBar style="light" />
 			<View style={styles.header}>
-				<TouchableOpacity onPress={work}>
-					<Text
-						style={{
-							...styles.btnText,
-							color: working ? theme.white : theme.grey,
-						}}
-					>
-						Work
-					</Text>
-				</TouchableOpacity>
-				<TouchableOpacity onPress={travle}>
-					<Text
-						style={{
-							...styles.btnText,
-							color: working ? theme.grey : theme.white,
-						}}
-					>
-						Travle
-					</Text>
-				</TouchableOpacity>
+				<Progress.Bar
+					style={{ marginBottom: 20 }}
+					color={theme.white}
+					progress={progress}
+					width={null}
+					animationType="spring"
+					borderWidth={0}
+				/>
+				<View
+					style={{
+						flexDirection: "row",
+						justifyContent: "space-between",
+					}}
+				>
+					<TouchableOpacity onPress={work}>
+						<Text
+							style={{
+								...styles.btnText,
+								color: working ? theme.white : theme.grey,
+							}}
+						>
+							Work
+						</Text>
+					</TouchableOpacity>
+					<TouchableOpacity onPress={travel}>
+						<Text
+							style={{
+								...styles.btnText,
+								color: working ? theme.grey : theme.white,
+							}}
+						>
+							Travel
+						</Text>
+					</TouchableOpacity>
+				</View>
 			</View>
 			<TextInput
 				onSubmitEditing={addToDo}
@@ -103,7 +189,7 @@ export default function App() {
 				keyboardAppearance="dark"
 				returnKeyType="done"
 			/>
-			{toDos === {} ? (
+			{toDos === {} ? ( // if toDos empty loading {} ? null ?
 				<View style={styles.loadingView}>
 					<ActivityIndicator
 						color={theme.white}
@@ -113,23 +199,65 @@ export default function App() {
 				</View>
 			) : (
 				<ScrollView>
-					{Object.keys(toDos).map((key) =>
-						toDos[key].working === working ? (
-							<View style={styles.toDo} key={key}>
-								<Text style={styles.toDoText}>
-									{toDos[key].text}
-								</Text>
-								<TouchableOpacity
-									onPress={() => deleteToDo(key)}
-								>
-									<Feather
-										name="trash-2"
-										size={24}
-										color={theme.grey}
+					{keyArray.map(
+						(
+							key // Object.keys 는 hashmap의 key 만 모아 배열로 반환해준다.
+						) =>
+							toDos[key].working === working ? ( // Work / Travle 탭을 true/false 로 구분
+								<View style={styles.toDo} key={key}>
+									<TouchableOpacity
+										style={{ padding: 1 }}
+										onPress={() => doneToDo(key)}
+									>
+										{toDos[key].done ? (
+											<Feather
+												name="check-square"
+												size={25}
+												color={theme.grey}
+											/>
+										) : (
+											<Feather
+												name="square"
+												size={25}
+												color={theme.white}
+											/>
+										)}
+									</TouchableOpacity>
+									<TextInput
+										style={{
+											...styles.editToDo,
+											textDecorationLine: toDos[key].done
+												? "line-through"
+												: "none",
+											color: toDos[key].done
+												? theme.grey
+												: theme.white,
+										}}
+										editable={
+											toDos[key].done ? false : true
+										}
+										onChangeText={(
+											text = toDos[key].text
+										) => {
+											if (!toDos[key].done)
+												changeToDo(key, text);
+										}}
+										value={toDos[key].text}
+										keyboardAppearance="dark"
+										returnKeyType="done"
+										placeholder="   "
 									/>
-								</TouchableOpacity>
-							</View>
-						) : null
+									<TouchableOpacity
+										onPress={() => deleteToDo(key)}
+									>
+										<Feather
+											name="trash-2"
+											size={24}
+											color={theme.grey}
+										/>
+									</TouchableOpacity>
+								</View>
+							) : null
 					)}
 				</ScrollView>
 			)}
@@ -144,8 +272,6 @@ const styles = StyleSheet.create({
 		paddingHorizontal: 20,
 	},
 	header: {
-		justifyContent: "space-between",
-		flexDirection: "row",
 		marginTop: 100,
 	},
 	btnText: {
@@ -168,7 +294,7 @@ const styles = StyleSheet.create({
 		justifyContent: "space-between",
 		backgroundColor: theme.toDoBg,
 		marginBottom: 10,
-		paddingVertical: 20,
+		paddingVertical: 12,
 		paddingHorizontal: 20,
 		borderRadius: 15,
 	},
@@ -179,4 +305,8 @@ const styles = StyleSheet.create({
 		alignItems: "center",
 	},
 	loadingCircle: { marginTop: -150 },
+	editToDo: {
+		color: theme.white,
+		fontSize: 16,
+	},
 });
